27c6b2580ac6d22f40c4f7af2ddcbe4a51acb676
Olanrewaju Olatunde <momoolatunde@outlook.com>
2023-02-19 21:28:29 +0000

    added get todo function

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,11 +1,11 @@
 
-const apiId = ''
+const apiId = 'll4oyi7ewl'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
 
-  domain: '...',
-  clientId: '...', 
+  domain: 'dev-3ag6nabvb85ne8fo.us.auth0.com',
+  clientId: 'CEs6asFRk9mxd47XdCTzRuVW5tQL3eMg', 
   callbackUrl: 'http://localhost:3000/callback'
 
 }
